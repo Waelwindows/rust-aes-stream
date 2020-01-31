65cc662a8ec33f9b5562c9e8778ff69f6b81b16f
Waelwindows <waelwindows@hotmail.com>
2020-02-01 01:26:30 +0300

    Refactor ECB and CBC wrappers to type aliases

--- src/aes_mode.rs
+++ src/aes_mode.rs
@@ -27,50 +27,20 @@ impl<T: BlockDecryptor, X: PaddingProcessor> AesMode for CbcDecryptor<T, X> {
     }
 }
 
-pub struct EcbReader<T: BlockDecryptor, X: PaddingProcessor, R: Read>(
-    AesReader<EcbDecryptor<T, X>, R>,
-);
-pub struct CbcReader<T: BlockDecryptor, X: PaddingProcessor, R: Read>(
-    AesReader<CbcDecryptor<T, X>, R>,
-);
+type EcbReader<B, P, R> = AesReader<EcbDecryptor<B, DecPadding<P>>, R>;
+type CbcReader<B, P, R> = AesReader<CbcDecryptor<B, DecPadding<P>>, R>;
 
 impl<T: BlockDecryptor, X: PaddingProcessor, R: Read> EcbReader<T, X, R> {
-    pub fn new(reader: R, algo: T, padding: X) -> Result<EcbReader<T, DecPadding<X>, R>> {
+    pub fn new_ecb(reader: R, algo: T, padding: X) -> Result<EcbReader<T, X, R>> {
         let bs = algo.block_size();
-        AesReader::new(reader, EcbDecryptor::new(algo, padding), bs).map(|a| Self(a))
+        AesReader::new(reader, EcbDecryptor::new(algo, padding), bs)
     }
-}
-impl<T: BlockDecryptor, X: PaddingProcessor, R: Read> CbcReader<T, X, R> {
-    pub fn new(mut reader: R, algo: T, padding: X) -> Result<CbcReader<T, DecPadding<X>, R>> {
+
+    pub fn new_cbc(mut reader: R, algo: T, padding: X) -> Result<CbcReader<T, X, R>> {
         let bs = algo.block_size();
         let mut iv = vec![0u8; bs];
         reader.read_exact(&mut iv)?;
 
-        AesReader::new(reader, CbcDecryptor::new(algo, padding, iv), bs).map(|a| Self(a))
-    }
-}
-use std::ops::{Deref, DerefMut};
-impl<T: BlockDecryptor, X: PaddingProcessor, R: Read> Deref for EcbReader<T, X, R> {
-    type Target = AesReader<EcbDecryptor<T, X>, R>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-impl<T: BlockDecryptor, X: PaddingProcessor, R: Read> DerefMut for EcbReader<T, X, R> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
-impl<T: BlockDecryptor, X: PaddingProcessor, R: Read> Deref for CbcReader<T, X, R> {
-    type Target = AesReader<CbcDecryptor<T, X>, R>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-impl<T: BlockDecryptor, X: PaddingProcessor, R: Read> DerefMut for CbcReader<T, X, R> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
+        AesReader::new(reader, CbcDecryptor::new(algo, padding, iv), bs)
     }
 }
